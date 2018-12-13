b103c5317f8f532232d4d0f6dbd6064237b921e1
LittleRookies <jiangmyhao@gmail.com>
2018-12-13 11:41:07 +0800

    SpringBoot-websocket

--- springboot-websocket/src/main/java/com/springbootdemo/springbootwebsocket/config/WebSocketConfig.java
+++ springboot-websocket/src/main/java/com/springbootdemo/springbootwebsocket/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.springbootdemo.springbootwebsocket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
@@ -10,7 +9,7 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  * @create 2018-12-11 15:22
  */
 @Configuration
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig{
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
