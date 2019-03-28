c02c74a11598962fcb32eca58f4706d27452fbd9
LittleRookies <jiangmyhao@gmail.com>
2019-03-28 17:03:48 +0800

    SpringBoot-jpa 自定义

--- springboot-jpa/src/main/java/com/springbootdemo/springbootjpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/springbootdemo/springbootjpa/SpringbootJpaApplication.java
@@ -1,9 +1,12 @@
 package com.springbootdemo.springbootjpa;
 
+import com.springbootdemo.springbootjpa.repository.impl.BaseRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.springbootdemo.springbootjpa.repository"}, repositoryBaseClass = BaseRepositoryImpl.class)
 public class SpringbootJpaApplication {
 
     public static void main(String[] args) {
---
+++ springboot-jpa/src/main/java/com/springbootdemo/springbootjpa/repository/MyPersonRepository.java
@@ -0,0 +1,6 @@
+package com.springbootdemo.springbootjpa.repository;
+
+import com.springbootdemo.springbootjpa.entity.PersonEntity;
+
+public interface MyPersonRepository extends BaseRepository<PersonEntity,Integer> {
+}
