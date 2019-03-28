f6b9d37f05909152145299de97636a6fb0783ee2
LittleRookies <44797612+LittleRookies@users.noreply.github.com>
2019-03-28 17:33:01 +0800

    Update BaseRepositoryImpl.java

--- springboot-jpa/src/main/java/com/springbootdemo/springbootjpa/repository/impl/BaseRepositoryImpl.java
+++ springboot-jpa/src/main/java/com/springbootdemo/springbootjpa/repository/impl/BaseRepositoryImpl.java
@@ -55,7 +55,7 @@ public class BaseRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRep
         Session session = (Session) entityManager.getDelegate();
         Query q = session.createNativeQuery(sql);
         q.unwrap(NativeQueryImpl.class).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
-        int i = 0;
+        int i = 1;
         for (Object arg : args
         ) {
             q.setParameter(i++, arg);
